c49a616144de38dc1e0ecc79690a556b6ec0a971
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-10-28 20:14:23 +0600

    The user's sign up code has been done in the SignUpActivity.

--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -2,10 +2,55 @@ package com.example.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.instagramclone.Models.User
+import com.example.instagramclone.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    lateinit var user: com.example.instagramclone.Models.User
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+        user = User()
+
+        binding.signUpBtn.setOnClickListener {
+            if (binding.nameET.text.toString() == "" && binding.emailET.text.toString() == "" && binding.passwordET.text.toString() == "") {
+                Toast.makeText(
+                    this@SignUpActivity,
+                    "Please fill up all the Information",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    binding.emailET.text.toString(), binding.passwordET.text.toString()
+                ).addOnCompleteListener { result ->
+                    if (result.isSuccessful) {
+                        user.name = binding.nameET.text.toString()
+                        user.email = binding.emailET.text.toString()
+                        user.password = binding.passwordET.text.toString()
+                        Firebase.firestore.collection("User")
+                            .document(Firebase.auth.currentUser!!.uid).set(user)
+                            .addOnSuccessListener {
+                                Toast.makeText(this@SignUpActivity, "Login", Toast.LENGTH_SHORT).show()
+                            }
+                    } else {
+                        Toast.makeText(
+                            this@SignUpActivity,
+                            result.exception?.localizedMessage,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+            }
+
+        }
     }
 }
