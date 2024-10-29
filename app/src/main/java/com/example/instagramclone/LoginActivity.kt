6b2d597c82d90e918596cb3e756a8abcef7566bb
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-10-29 11:40:00 +0600

    LoginActivity backend part completed.

--- app/src/main/java/com/example/instagramclone/LoginActivity.kt
+++ app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -1,11 +1,49 @@
 package com.example.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.instagramclone.Models.User
+import com.example.instagramclone.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            if (binding.emailET.text.toString() == "" && binding.passwordET.text.toString() == "") {
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Please fill up all the Information",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            } else {
+                var user = User(binding.emailET.text.toString(), binding.passwordET.text.toString())
+                Firebase.auth.signInWithEmailAndPassword(user.email!!, user.password!!)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
+                            finish()
+
+                        } else {
+                            Toast.makeText(
+                                this@LoginActivity,
+                                it.exception?.localizedMessage,
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                    }
+            }
+        }
     }
 }
